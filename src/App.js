f1a69ea7e511afe49cfbc25165f2dd4ca025ebc5
Neha Sharma <Neha Sharma@LAPTOP-C0L7II0J>
2020-11-21 16:39:24 +0530

    scroll added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import CardList from './CardList'
 import {robots} from './robots';
+import Scroll from './Scroll'
 function App(){ 
     const [searchRobot, setSearchRobot] = useState('');
+    //const [robots, setRobots] = useState([]);
     const handleSearch = (e) =>{
         setSearchRobot(e.target.value);
         console.log(searchRobot);
@@ -15,8 +17,11 @@ function App(){
                 <h1>RoboFriends</h1>
                 <div className="pa2">
             <input type="search" onChange={handleSearch} value={searchRobot} placeholder="Search Robots" className="pa3 ba b--green bg-lightest-blue"/>
-        </div>
-                <CardList robots={filterName}/>  
+                </div>
+                <Scroll>
+                    <CardList robots={filterName}/>  
+                </Scroll>
+               
             </div>
         );
    
